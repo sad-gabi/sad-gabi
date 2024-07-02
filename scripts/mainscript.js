4298236e08758f2d3a91a96cffd5f0e25e799739
Sad Gabi <sadgabi20@gmail.com>
2024-07-01 22:51:36 -0600

    d

--- scripts/mainscript.js
+++ scripts/mainscript.js
@@ -1,7 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
     const body = document.body;
     const main = document.getElementById('main');
+    const navBar = document.getElementById('navBar');
 
+    body.style.height = window.innerHeight + 'px';
 
     setTimeout(() => {
         main.style.height = window.innerHeight + 'px';
@@ -10,5 +12,19 @@ document.addEventListener('DOMContentLoaded', () => {
             main.style.borderBottomLeftRadius = '0';
             main.style.borderBottomRightRadius = '0';
         }, 500);
+
+        setTimeout(() => {
+            navBar.style.opacity = '1';
+            navBar.style.pointerEvents = 'all';
+
+            document.body.style.backgroundImage = 'none';
+            document.body.style.backgroundColor = 'var(--main-color)';
+        }, 1000);
     }, 1000);
+
+
+    document.addEventListener('resize', () => {
+        body.style.height = window.innerHeight + 'px';
+        main.style.height = window.innerHeight + 'px';
+    })
 })
